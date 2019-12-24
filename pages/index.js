ac7553d435f2f2fce5ab6ec9f758fcd347931825
Kyle Kochanek <the_designer@kylekochanek.com>
2019-12-24 12:59:53 -0500

    Migrated the footer into its own componant

--- pages/index.js
+++ pages/index.js
@@ -3,79 +3,56 @@ import Link from 'next/link';
 import Head from '../components/head';
 import Nav from '../components/nav';
 import css from '../styles/main.scss';
+import Footer from '../components/footer';
 // import BlogList from '../components/BlogList';
 // import Layout from '../components/Layout';
 
-const Home = () => {
-  const [date, setDate] = useState(null);
+const Home = () => (
+  <div>
+    <Head title="Home" />
+    <Nav />
 
-  useEffect(() => {
-    async function getDate() {
-      const res = await fetch('/api/date');
-      const newDate = await res.json();
+    <div className="hero">
+      <h1 className={css.header}>
+        Kyle is a new dad, product designer, front-end developer, and a
+        voracious learner.
+      </h1>
+      <h2 className={css.title}>
+        Currently the senior product designer, at Roadtrippers 🗺. Freelance
+        designer and coffee nerd. Feel free to reach out! Lets talk about
+        pixels, maps, or coffee ✌️
+      </h2>
 
-      setDate(newDate);
-    }
-    getDate();
-  }, [setDate]);
-
-  return (
-    <div>
-      <Head title="Home" />
-      <Nav />
-
-      <div className="hero">
-        <h1 className={css.header}>
-          Kyle is a new dad, product designer, front-end developer, and a
-          voracious learner.
-        </h1>
-        <h2 className={css.title}>
-          Currently the senior product designer, at Roadtrippers 🗺. Freelance
-          designer and coffee nerd. Feel free to reach out! Lets talk about
-          pixels, maps, or coffee ✌️
-        </h2>
-
-        <p className={css.body}>
-          The date is:&nbsp;{' '}
-          {date ? (
-            <span>
-              <b>{date.date}</b>
-            </span>
-          ) : (
-            <span className="loading" />
-          )}
-        </p>
-
-        <div className="row">
-          <Link href="https://github.com/zeit/next.js#setup">
-            <a className="card">
-              <h3>Getting Started &rarr;</h3>
-              <p>Learn more about Next.js on GitHub and in their examples.</p>
-            </a>
-          </Link>
-          <Link href="https://github.com/zeit/next.js/tree/master/examples">
-            <a className="card">
-              <h3>Examples &rarr;</h3>
-              <p>Find other example boilerplates on the Next.js GitHub.</p>
-            </a>
-          </Link>
-          <Link href="https://github.com/zeit/next.js">
-            <a className="card">
-              <h3>Create Next App &rarr;</h3>
-              <p>Was this tool helpful? Let us know how we can improve it!</p>
-            </a>
-          </Link>
-        </div>
+      <div className="row">
+        <Link href="https://github.com/zeit/next.js#setup">
+          <a className="card">
+            <h3>Getting Started &rarr;</h3>
+            <p>Learn more about Next.js on GitHub and in their examples.</p>
+          </a>
+        </Link>
+        <Link href="https://github.com/zeit/next.js/tree/master/examples">
+          <a className="card">
+            <h3>Examples &rarr;</h3>
+            <p>Find other example boilerplates on the Next.js GitHub.</p>
+          </a>
+        </Link>
+        <Link href="https://github.com/zeit/next.js">
+          <a className="card">
+            <h3>Create Next App &rarr;</h3>
+            <p>Was this tool helpful? Let us know how we can improve it!</p>
+          </a>
+        </Link>
       </div>
-      <div>
+    </div>
+    <Footer />
+    {/* <div>
         <p>
           © {date ? <span>{date.date}</span> : <span className="loading" />},
           Kyle Kochanek. Site deployed on <a href="https://zeit.co/">NOW</a> &
           font provided by <a href="https://github.com/rsms/inter">Inter</a>
         </p>
-      </div>
-    </div>
-  );
-};
+      </div> */}
+  </div>
+);
 
 export default Home;
