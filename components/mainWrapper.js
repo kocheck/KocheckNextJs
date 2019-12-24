a18a814a9c064e187cedf835e8ad203e573cbfe4
Kyle Kochanek <the_designer@kylekochanek.com>
2019-12-24 10:06:04 -0500

    Typogrpahy updates. Making everything all modular

---
+++ components/mainWrapper.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Main = () => (
+  <main>
+    <h1>Hello</h1>
+  </main>
+);
+
+export default Main;
--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 import Link from 'next/link';
 import Head from '../components/head';
 import Nav from '../components/nav';
-import css from '../styles/typography.scss';
+import css from '../styles/main.scss';
 // import BlogList from '../components/BlogList';
 // import Layout from '../components/Layout';
 
@@ -24,15 +24,15 @@ const Home = () => {
       <Nav />
 
       <div className="hero">
-        <h1 className={css.example}>
+        <h1 className={css.header}>
           Kyle is a new dad, product designer, front-end developer, and a
           voracious learner.
         </h1>
-        <p className="description">
+        <h2 className={css.title}>
           Currently the senior product designer, at Roadtrippers 🗺. Freelance
           designer and coffee nerd. Feel free to reach out! Lets talk about
           pixels, maps, or coffee ✌️
-        </p>
+        </h2>
 
         <p className="row date">
           The date is:&nbsp;{' '}
@@ -44,16 +44,6 @@ const Home = () => {
             <span className="loading" />
           )}
         </p>
-
-        {/* <Layout
-          pathname="/"
-          siteTitle={props.title}
-          siteDescription={props.description}
-        >
-          <section>
-            <BlogList allBlogs={props.allBlogs} />
-          </section>
-        </Layout> */}
 
         <div className="row">
           <Link href="https://github.com/zeit/next.js#setup">
@@ -76,83 +66,6 @@ const Home = () => {
           </Link>
         </div>
       </div>
-
-      <style jsx>{`
-        .hero {
-          width: 100%;
-          color: #333;
-        }
-        .title {
-          margin: 0;
-          width: 100%;
-          padding-top: 80px;
-          line-height: 1.15;
-          font-size: 48px;
-        }
-        .title,
-        .description {
-          text-align: center;
-        }
-        .row {
-          max-width: 880px;
-          margin: 80px auto 40px;
-          display: flex;
-          flex-direction: row;
-          justify-content: space-around;
-        }
-        .date {
-          height: 24px;
-          max-width: calc(100% - 32px)
-          text-align: center;
-          display: flex;
-          align-items: center;
-          justify-content: center;
-          padding: 0 16px;
-        }
-        .date p {
-          text-align: center;
-        }
-        .date span {
-          width: 176px;
-          text-align: center;
-        }
-        @keyframes Loading {
-          0%{background-position:0% 50%}
-          50%{background-position:100% 50%}
-          100%{background-position:0% 50%}
-        }
-        .date .loading {
-          max-width: 100%;
-          height: 24px;
-          border-radius: 4px;
-          display: inline-block;
-          background: linear-gradient(270deg, #D1D1D1, #EAEAEA);
-          background-size: 200% 200%;
-          animation: Loading 2s ease infinite;
-        }
-        .card {
-          padding: 18px 18px 24px;
-          width: 220px;
-          text-align: left;
-          text-decoration: none;
-          color: #434343;
-          border: 1px solid #9b9b9b;
-        }
-        .card:hover {
-          border-color: #067df7;
-        }
-        .card h3 {
-          margin: 0;
-          color: #067df7;
-          font-size: 18px;
-        }
-        .card p {
-          margin: 0;
-          padding: 12px 0 0;
-          font-size: 13px;
-          color: #333;
-        }
-      `}</style>
     </div>
   );
 };
